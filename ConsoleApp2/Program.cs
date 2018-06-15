622c0c55a6b0e7e6bee524468b40dfaad27212ab
marco.ferreira <marco.ferreira@dtidigital.com.br>
2018-06-15 09:15:15 -0300

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        /*
+         * Um palíndromo é um número que, ao ser invertido, pode ser lido da mesma maneira. 
+         * Exemplo: 2002, 12321, 4554...
+         * Implemente a função que verifica se um determinado número é um palíndromo
+         */
+        public static bool VerificarPalindromo(int numero)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+         * O maior palíndromo obtido pela multiplicação de dois números de dois algorítimos é 9009, 
+         * obtido ao se multiplicar 99 X 91 = 9009.
+         * Implemente a função para calcular o maior palíndromo obtido pela multiplicação de dois números
+         *  de três ou mais algarísmos. A função deverá receber o número de algarísmos dos operandos.
+         */
+        public static int MaiorPalindromo(int numeroDeDigitos)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+         * O número 197 é chamado primo circular porque todas as suas rotações de digitos também são primos: 197, 971, e 719.
+         * Implemente uma função para verificar se um dado número é primo circular
+         */
+        public static bool VerificarNumeroCircularPrimo(int numero)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
